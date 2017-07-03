fe6d074e5a67845f6516b6d3ce205650920035d3
Edmond <edmondcheung604@hotmail.com>
2017-07-03 18:57:17 +0000

    Printed html in chunks

---
+++ print-in-chunks.js
@@ -0,0 +1,19 @@
+var https = require("https");
+
+function getAndPrintHTMLChunks () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  /* Add your code here */
+https.get(requestOptions, function (response) {
+  response.on('data', function (data) {
+    data += data.length
+    console.log(data + '\n');
+  });
+});
+}
+
+getAndPrintHTMLChunks();
